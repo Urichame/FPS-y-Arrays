fcf53ba2b718f4fd2a3c280b714eefdda1cf4e4a
Urichame <143609985+Urichame@users.noreply.github.com>
2024-06-06 09:06:40 -0300

    Initial commit

---
+++ Assets/Standard Assets/Personajes.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personajes : MonoBehaviour
+{
+    public int nivelHambre;
+    public Alimento alimento;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void TomarAlimento(int valorAlimentario)
+    {
+        nivelHambre -= valorAlimentario;
+
+        if (nivelHambre <= 0)
+        {
+            nivelHambre = 0;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+        {
+            Debug.Log("trigger");
+            Alimento alimento;
+            alimento = other.GetComponent<Alimento>();
+        TomarAlimento(alimento.valorAlimentario);
+            
+        }
+
+    }
